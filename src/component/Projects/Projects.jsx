15649f2c6a34bec621c6f48fcddadc943b2e6277
shivank mishra <shivankmishra710@gmail.com>
2024-03-07 11:39:39 +0530

    removed data.js and project cards due to image path  not working correctly in vercel

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
-import ProjectsCards from '../ProjectsCards/ProjectsCards';
-import Data from '@/Data';
-import './project.css';
+import { Link } from 'react-router-dom';
+import github from "@/src/assets/images/github-icon.png";
+import codeicon from "@/src/assets/images/icon-code-0.jpg";
+import image1 from '@/src/assets/images/cardsImage/keeperApp.png';
+import image2 from '@/src/assets/images/cardsImage/currencyConverter.png';
+import image3 from '@/src/assets/images/cardsImage/passwordGenrator.png';
+
 function Projects() {
-  return (
-    <>
-    <div className='container flex justify-center'>
-      {Array.from({ length: 3 }).map((_, index) => (
-        <ProjectsCards 
-          key={index} // Adding a unique key for each iteration
-          projectName={Data[index].projectName}
-          Linking={Data[index].Linking}
-          image={Data[index].image}
-          githubLink={Data[index].githubLink}
-        />
-    
-      ))}
+  const ProjectCard = ({ projectName, Linking, image, githubLink }) => (
+    <div className='flex flex-col items-center border border-black rounded-2xl m-5 p-5'>
+      <img className="h-48 w-48 rounded-2xl" src={image} alt="Project Image" />
+      <h1 className='text-xl mt-4 mb-2'>{projectName}</h1>
+
+      <div className='flex items-center justify-center w-full my-4 font-bold'>
+        <button><Link to={githubLink}><img className='h-10 m-1' src={github} alt="GitHub Logo" /></Link></button>
+        <button><Link to={Linking}><img className='h-10 m-1' src={codeicon} alt="Code Icon" /></Link></button>
       </div>
-    </>
+    </div>
+  );
+
+  return (
+    <div className='container mx-auto flex flex-wrap justify-center'>
+      <ProjectCard 
+        projectName="my own keeperapp"
+        Linking="https://my-own-keeperapp.vercel.app/"
+        image={image1}
+        githubLink="https://github.com/ShivankKMishra/myOwnKeeperapp"
+      />
+      <ProjectCard 
+        projectName="currency-converter"
+        Linking="https://currency-converter-react-phi.vercel.app/"
+        image={image2}
+        githubLink="https://github.com/ShivankKMishra/currencyConverter-react"
+      />
+      <ProjectCard 
+        projectName="password Genrator"
+        Linking="https://password-genrator-jet.vercel.app/"
+        image={image3}
+        githubLink="https://github.com/ShivankKMishra/passwordGenrator"
+      />
+    </div>
   );
 }
 
 export default Projects;
-
