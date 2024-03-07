7683a6604ea06365c584b3d6df19ba4698d172e5
shivank mishra <shivankmishra710@gmail.com>
2024-03-07 09:56:51 +0530

    ....

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-
+import image from "@/src/assets/images/image.png";
 export default function Header() {
   return (
     <header className="">
       <nav className="flex justify-between py-4 ">
         <div>
           <div className=' px-10 py-3'>
-            <Link to="/" className=""><img className="h-12" src="src/assets/images/image.png" alt="Logo" /></Link>
+            <Link to="/" className=""><img className="h-12" src={image} alt="Profile Picture" /></Link>
           </div>
         </div>
 
